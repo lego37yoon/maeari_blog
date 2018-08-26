d729fc7dc2024865fd4a766cbf6e9807b47bb069
윤정민(종이상자) <lego37yoon@outlook.com>
2018-08-26 21:30:43 +0900

    Spinner for Blog API Select Screen.

--- app/src/main/java/pw/pbdiary/maeari/blog/AddExBlogS2.java
+++ app/src/main/java/pw/pbdiary/maeari/blog/AddExBlogS2.java
@@ -1,8 +1,9 @@
 package pw.pbdiary.maeari.blog;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddExBlogS2 extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class AddExBlogS2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addexblogs2);
+
+        Spinner api_select_spinner = (Spinner)findViewById(R.id.api_select_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.api_value, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        api_select_spinner.setAdapter(adapter);
     }
 }
