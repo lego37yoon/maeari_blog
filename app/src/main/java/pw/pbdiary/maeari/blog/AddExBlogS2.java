eaa008724fe9a00b9dabbc8ac773a7ae55e0cd0f
윤정민(종이상자) <lego37yoon@outlook.com>
2018-08-27 19:32:51 +0900

    Add MetaWeBlog Auth Form Activity Support for android.support.PARENT_ACTIVITY

--- app/src/main/java/pw/pbdiary/maeari/blog/AddExBlogS2.java
+++ app/src/main/java/pw/pbdiary/maeari/blog/AddExBlogS2.java
@@ -1,18 +1,34 @@
 package pw.pbdiary.maeari.blog;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 
 public class AddExBlogS2 extends AppCompatActivity {
+    private Button apiTistory;
+    private Button apiEgloos;
+    private Button apiMWBlog;
 
+    private static final int API_OPTION_METAWE = 0;
+    private static final int API_OPTION_TISTORY = 1;
+    private static final int API_OPTION_EGLOOS = 2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addexblogs2);
 
-        Spinner api_select_spinner = (Spinner)findViewById(R.id.api_select_spinner);
+        apiTistory = (Button) findViewById(R.id.tisOAuthButton);
+        apiEgloos = (Button) findViewById(R.id.eglooOAuthButton);
+        apiMWBlog = (Button) findViewById(R.id.mwbAuthButton);
+
+        final Spinner api_select_spinner = (Spinner)findViewById(R.id.api_select_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.api_value, android.R.layout.simple_spinner_item);
@@ -20,5 +36,43 @@ public class AddExBlogS2 extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
         api_select_spinner.setAdapter(adapter);
+
+        api_select_spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id)
+            {
+                switch(position) {
+                    case API_OPTION_METAWE: {
+                        apiMWBlog.setVisibility(View.VISIBLE);
+                        apiEgloos.setVisibility(View.GONE);
+                        apiTistory.setVisibility(View.GONE);
+                    }
+                    case API_OPTION_TISTORY: {
+                        apiMWBlog.setVisibility(View.GONE);
+                        apiEgloos.setVisibility(View.GONE);
+                        apiTistory.setVisibility(View.VISIBLE);
+                    }
+                    case API_OPTION_EGLOOS: {
+                        apiMWBlog.setVisibility(View.GONE);
+                        apiEgloos.setVisibility(View.VISIBLE);
+                        apiTistory.setVisibility(View.GONE);
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                apiMWBlog.setVisibility(View.GONE);
+                apiEgloos.setVisibility(View.GONE);
+                apiTistory.setVisibility(View.GONE);
+            }
+        });
+    }
+
+    public void gotoMWBlogAuthForm(View view) {
+        Intent goMWBlogAuth = new Intent(getApplicationContext(), MWBlogAuthForm.class);
+
+        startActivity(goMWBlogAuth);
     }
 }
---
+++ app/src/main/java/pw/pbdiary/maeari/blog/MWBlogAuthForm.java
@@ -0,0 +1,14 @@
+package pw.pbdiary.maeari.blog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MWBlogAuthForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mwblog_auth_form);
+    }
+}
